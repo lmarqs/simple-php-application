6456784eb45447296be48f48c90ae42b2a4c6522
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-08-12 22:55:56 -0300

    Implement ContactRoute

--- src/main/php/Middleware/Route/Api/ApiRoute.php
+++ src/main/php/Middleware/Route/Api/ApiRoute.php
@@ -7,24 +7,6 @@ class ApiRoute extends Handler
 {
     public function __construct()
     {
-
-        $this->add('', function ($request, $response, $next) {
-            $response->write('filter');
-            $next();
-            return;
-
-            if (session_status() == PHP_SESSION_ACTIVE && $_SESSION['logged_in'] == true) {
-                $next();
-            } else {
-                $response->send(404);
-            }
-        });
-
-        $this->add('', function ($request, $response, $next) {
-            $response->write('api');
-            $next();
-        });
-
         $this->add('contact', new ContactRoute());
     }
 }
--- src/main/php/Middleware/Route/ContactRoute.php
+++ src/main/php/Middleware/Route/ContactRoute.php
@@ -1,6 +1,7 @@
 <?php
 namespace lmarqs\Spa\Middleware\Route;
 
+use lmarqs\Spa\Controller\ContactController;
 use lmarqs\Spa\Middleware\Handler;
 use lmarqs\Spa\Middleware\Request;
 
@@ -8,25 +9,27 @@ class ContactRoute extends Handler
 {
     public function __construct()
     {
+
+        $this->add('', function ($request, $response, $next) {
+            if ($request->method() == Request::METHOD_GET || $request->method() == Request::METHOD_POST) {
+                $next();
+            } else {
+                $response->send(405);
+            }
+        });
+
+        $this->add('create', function ($request, $response, $next) {
+            $request->setAttribute('id', '0');
+            $next();
+        });
+
         $this->add('([0-9]+)', function ($request, $response, $next, $matches) {
-            $response
-                ->write($request->method())
-                ->write('contact/')
-                ->write($matches[0])
-                ->send();
+            $request->setAttribute('id', $matches[0]);
+            $next();
         });
 
         $this->add('', function ($request, $response, $next, $matches) {
-
-            if ($request->method() != Request::METHOD_GET) {
-                $response->send(405);
-                return;
-            }
-
-            $response
-                ->write('contacts')
-                ->write($matches[0])
-                ->send();
+            ContactController::processRequest($request, $response);
         });
 
     }
