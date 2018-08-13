2afcd775c91d9e7db51a23db3d0e41963e710d72
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-08-12 22:56:53 -0300

    Create navbar

---
+++ src/main/php/view/navbar.tpl.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-light bg-light navbar-expand-lg">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/logout">Logout</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
