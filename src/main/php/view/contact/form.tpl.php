8c265caa1890c09938a36626881c6bd836c51ffd
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-08-13 20:42:13 -0300

    Use htmlspecialchars

--- src/main/php/view/contact/form.tpl.php
+++ src/main/php/view/contact/form.tpl.php
@@ -2,29 +2,29 @@
 <br/>
 <div class="card">
     <div class="card-body">
-        <h5 class="card-title"><?=$attr['id'] == null ? 'Creating' : 'Updating'?> a contact</h5>
+        <h5 class="card-title"><?=$attrs["id"] == null ? "Creating" : "Updating"?> a contact</h5>
         <a href="/contact" class="btn btn-primary">Contacts</a>
         <br/>
         <br/>
         <form role="form" class="form-horizontal" method="post" autocomplete="off">
             <div class="form-group">
                 <label>Name</label>
-                <input name="name" type="text" required class="form-control <?=isset($errs['name']) ? 'is-invalid' : ''?>">
-                <div class="invalid-feedback"><?=$errs['name']?></div>
+                <input name="name" type="text" required value="<?=htmlspecialchars($attrs["name"])?>" class="form-control <?=isset($errs["name"]) ? "is-invalid" : ""?>">
+                <div class="invalid-feedback"><?=htmlspecialchars($errs["name"])?></div>
             </div>
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
                         <label>Phone</label>
-                        <input name="phone" type="tel" class="form-control <?=isset($errs['phone']) ? 'is-invalid' : ''?>">
-                        <div class="invalid-feedback"><?=$errs['phone']?></div>
+                        <input name="phone" type="tel" class="form-control <?=isset($errs["phone"]) ? "is-invalid" : ""?>">
+                        <div class="invalid-feedback"><?=htmlspecialchars($errs["phone"])?></div>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label>Email</label>
-                        <input name="email" type="email" class="form-control <?=isset($errs['email']) ? 'is-invalid' : ''?>">
-                        <div class="invalid-feedback"><?=$errs['email']?></div>
+                        <input name="email" type="email" class="form-control <?=isset($errs["email"]) ? "is-invalid" : ""?>">
+                        <div class="invalid-feedback"><?=htmlspecialchars($errs["email"])?></div>
                     </div>
                 </div>
             </div>
@@ -32,8 +32,8 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label>Birthday</label>
-                        <input name="birthday" type="date" class="form-control <?=isset($errs['birthday']) ? 'is-invalid' : ''?>">
-                        <div class="invalid-feedback"><?=$errs['birthday']?></div>
+                        <input name="birthday" type="date" class="form-control <?=isset($errs["birthday"]) ? "is-invalid" : ""?>">
+                        <div class="invalid-feedback"><?=htmlspecialchars($errs["birthday"])?></div>
                     </div>
                 </div>
             </div>
