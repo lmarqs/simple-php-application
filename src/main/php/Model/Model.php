1f45ea46ad55aea44ab91c5ad8c005b2c795a0bf
Lucas Marques de Paula <lucas.marques@objective.com.br>
2018-08-14 07:50:45 -0300

    Add id access to model

--- src/main/php/Model/Model.php
+++ src/main/php/Model/Model.php
@@ -3,6 +3,8 @@ namespace lmarqs\Spa\Model;
 
 abstract class Model
 {
+    abstract public function getId();
+    abstract public function setId($id);
     abstract public function toArray();
     abstract public function fromArray($array);
 }
